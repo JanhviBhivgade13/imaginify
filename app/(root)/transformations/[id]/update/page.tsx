4e79ef2368c5b997ea05fbbc2bdc6b258bd52e84
JanhviBhivgade13 <149391061+JanhviBhivgade13@users.noreply.github.com>
2024-02-23 19:52:51 +0530

    initial commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformationPge = () => {
+  return (
+    <div>
+      UpdateTransformationPge
+    </div>
+  )
+}
+
+export default UpdateTransformationPge
